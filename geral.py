3e03e91ea5b307961791fbe653c339f95e10b538
Calebe Melo <calebemeloaraujo07@gmail.com>
2024-12-17 18:49:26 -0400

    Adicionei exceções personalizadas para tratar entradas inválidas no cadastro de atividades. Usei try-except para melhorar o controle de erros e facilitar a manutenção do código.

--- geral.py
+++ geral.py
@@ -5,6 +5,12 @@ from cor import azul, fim
 import os
 import time
 
+class TipoAtividadeInvalido(Exception):
+    pass
+
+class EntradaInvalida(Exception):
+    pass
+
 def limpar_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -14,92 +20,99 @@ def main():
 
     while True:
         limpar_terminal()
-        escolha = input(f'''Escolha uma opção:
+        try:
+            escolha = input(f'''Escolha uma opção:
 {azul}[1]Cadastrar   [2]Login{fim}
 Resposta: ''')
 
-        if escolha == "1":
-            limpar_terminal()
-            matricula = input("\nMatrícula: ")
-            senha = input("Senha: ")
-            sistema.cadastrar_usuario(matricula, senha)
-        elif escolha == "2":
-            limpar_terminal()
-            matricula = input("\nMatrícula: ")
-            senha = input("Senha: ")
-            if sistema.login(matricula, senha):
-                time.sleep(1.5)
+            if escolha == "1":
                 limpar_terminal()
-
-                print(f"\n{azul}Cadastro de Aluno{fim}")
-                nome = input("Nome completo: ")
-                idade = input("Idade: ")
-                turma = input("Turma: ")
-                curso = input("Curso: ")
-                tipo_grade = input("Tipo de Grade: ")
-                aluno = Aluno(nome, idade, turma, curso, tipo_grade, matricula)
-
-                while True:
+                matricula = input("\nMatrícula: ")
+                senha = input("Senha: ")
+                sistema.cadastrar_usuario(matricula, senha)
+            elif escolha == "2":
+                limpar_terminal()
+                matricula = input("\nMatrícula: ")
+                senha = input("Senha: ")
+                if sistema.login(matricula, senha):
+                    time.sleep(1.5)
                     limpar_terminal()
-                    print("\nGerenciamento de Atividades")
-                    print(f"{azul}[1]Adicionar Atividade{fim}")
-                    print(f"{azul}[2]Exibir Atividades{fim}")
-                    print(f"{azul}[3]Sair{fim}")
-                    escolha_atividade = input("\nEscolha uma opção: ")
 
-                    if escolha_atividade == "1":
+                    print(f"\n{azul}Cadastro de Aluno{fim}")
+                    nome = input("Nome completo: ")
+                    idade = input("Idade: ")
+                    turma = input("Turma: ")
+                    curso = input("Curso: ")
+                    tipo_grade = input("Tipo de Grade: ")
+                    aluno = Aluno(nome, idade, turma, curso, tipo_grade, matricula)
+
+                    while True:
                         limpar_terminal()
-                        tipo = int(input(f'''\nTipo de Atividade
+                        print("\nGerenciamento de Atividades")
+                        print(f"{azul}[1]Adicionar Atividade{fim}")
+                        print(f"{azul}[2]Exibir Atividades{fim}")
+                        print(f"{azul}[3]Sair{fim}")
+                        escolha_atividade = input("\nEscolha uma opção: ")
+
+                        if escolha_atividade == "1":
+                            limpar_terminal()
+                            tipo = int(input(f'''\nTipo de Atividade
 {azul}[1]Tarefa   [2]Projeto   [3]Trabalho   [4]Prova{fim}
 Resposta: '''))
-                        
-                        # Mapeamento de números para tipos de atividade
-                        tipos_validos = {1: 'Tarefa', 2: 'Projeto', 3: 'Trabalho', 4: 'Prova'}
-                        tipo_str = tipos_validos.get(tipo)
 
-                        if tipo_str:  # Se o tipo for válido
-                            titulo = input("\nTítulo: ")
-                            descricao = input("Descrição: ")
-                            data_entrega = input("Data de Entrega: ")
+                            tipos_validos = {1: 'Tarefa', 2: 'Projeto', 3: 'Trabalho', 4: 'Prova'}
+                            tipo_str = tipos_validos.get(tipo)
 
-                            if tipo_str == "Tarefa":
-                                disciplina = input("Disciplina: ")
-                                atividade = Tarefa(titulo, descricao, data_entrega, disciplina)
-                            elif tipo_str == "Projeto":
-                                tipo_projeto = input("Tipo de Projeto: ")
-                                professor_orientador = input("Professor Orientador: ")
-                                data_inicio = input("Data de Início: ")
-                                atividade = Projeto(titulo, descricao, data_entrega, tipo_projeto, professor_orientador, data_inicio)
-                            elif tipo_str == "Trabalho":
-                                disciplina = input("Disciplina: ")
-                                atividade = Trabalho(titulo, descricao, data_entrega, disciplina)
-                            elif tipo_str == "Prova":
-                                disciplina = input("Disciplina: ")
-                                horario = input("Horário: ")
-                                tentativa = input("Tentativa: ")
-                                atividade = Prova(titulo, descricao, data_entrega, disciplina, horario, tentativa)
+                            if tipo_str:
+                                titulo = input("\nTítulo: ")
+                                descricao = input("Descrição: ")
+                                data_entrega = input("Data de Entrega: ")
 
-                            gerenciador.adicionar_atividade(tipo_str, atividade)
-                            time.sleep(2)
+                                if tipo_str == "Tarefa":
+                                    disciplina = input("Disciplina: ")
+                                    atividade = Tarefa(titulo, descricao, data_entrega, disciplina)
+                                elif tipo_str == "Projeto":
+                                    tipo_projeto = input("Tipo de Projeto: ")
+                                    professor_orientador = input("Professor Orientador: ")
+                                    data_inicio = input("Data de Início: ")
+                                    atividade = Projeto(titulo, descricao, data_entrega, tipo_projeto, professor_orientador, data_inicio)
+                                elif tipo_str == "Trabalho":
+                                    disciplina = input("Disciplina: ")
+                                    atividade = Trabalho(titulo, descricao, data_entrega, disciplina)
+                                elif tipo_str == "Prova":
+                                    disciplina = input("Disciplina: ")
+                                    horario = input("Horário: ")
+                                    tentativa = input("Tentativa: ")
+                                    atividade = Prova(titulo, descricao, data_entrega, disciplina, horario, tentativa)
+
+                                gerenciador.adicionar_atividade(tipo_str, atividade)
+                                time.sleep(2)
+                            else:
+                                raise TipoAtividadeInvalido("Tipo inválido. Tente novamente.")
+                            
+                        elif escolha_atividade == "2":
+                            limpar_terminal()
+                            print(f"{azul}\nAtividades Cadastradas:{fim}")
+                            gerenciador.exibir_atividades()
+                            input(f"{azul}\nPressione ENTER para continuar...{fim}")
+
+                        elif escolha_atividade == "3":
+                            print("\nSaindo...")
+                            time.sleep(1)
+                            break
                         else:
-                            print("Tipo inválido. Tente novamente.")
-                            time.sleep(1.5)
-
-                    elif escolha_atividade == "2":
-                        limpar_terminal()
-                        print(f"{azul}\nAtividades Cadastradas:{fim}")
-                        gerenciador.exibir_atividades()
-                        input(f"{azul}\nPressione ENTER para continuar...{fim}")
-
-                    elif escolha_atividade == "3":
-                        print("\nSaindo...")
-                        time.sleep(1)
-                        break
-                    else:
-                        print("Escolha inválida. Tente novamente.")
-                        time.sleep(1.5)
-            else:
-                input(f"\n{azul}Login falhou. Pressione ENTER para tentar novamente.{fim}")
+                            raise EntradaInvalida("Escolha inválida. Tente novamente.")
+                else:
+                    input(f"\n{azul}Login falhou. Pressione ENTER para tentar novamente.{fim}")
+        except TipoAtividadeInvalido as e:
+            print(e)
+            time.sleep(1.5)
+        except EntradaInvalida as e:
+            print(e)
+            time.sleep(1.5)
+        except Exception as e:
+            print(f"Erro inesperado: {e}")
+            time.sleep(1.5)
 
 if __name__ == "__main__":
     main()
